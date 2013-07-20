1a621b05064a65f7371c6f5888095a859b5ed36b
Mohammed El-Bachiri <mohammed.elbachiri@gmail.com>
2013-07-20 18:38:17 +0200

    Update Gauss.cc

--- Gauss.cc
+++ Gauss.cc
@@ -4,7 +4,5 @@ Gauss::Gauss(uint _seed) : seed(_seed), rng(seed){
 	boost::variate_generator<mt19937, DIST> generator(rng, DIST(0.0, 1.0));
 }
 
-double Gauss::operator()() const{
-	return generator();
-}
+double Gauss::operator()() const{ return generator(); }
 
