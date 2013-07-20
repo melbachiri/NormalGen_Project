a7714d5dda3218f35cb227779421619159cf5fc6
Mohammed El-Bachiri <mohammed.elbachiri@gmail.com>
2013-07-20 18:37:25 +0200

    Update Gauss.h

--- Gauss.h
+++ Gauss.h
@@ -1,25 +1,23 @@
 #ifndef __NORMAL__
 #define __NORMAL__
 
-#include <boost/random.hpp>
-#include <time.h>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/variate_generator.hpp>
 
 typedef unsigned int uint;
+typedef boost::mt19937 mt19937;
+typedef boost::normal_distribution<double> DIST;
+
 
 class Gauss{
 public:
-	Gauss(double _mean = 0.0, double _variance = 1.0, uint _seed = 0);
+	Gauss(uint _seed = 2013);
 	double operator()() const;
 
 private:
-	double mean, variance;
 	uint seed;
-	boost::mt19937 rng;	// Générateur Mers. twistter
-	boost::normal_distribution<double> dist_normal;		// la distribution
-	boost::variate_generator<boost::mt19937, boost::normal_distribution<double> > generator;
+	mt19937 rng;
 };
 
 #endif
